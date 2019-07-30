62ad1a01425e3cbccbfa9f401fedb0ce32486c0e
duc <ducxo98@gmail.com>
2019-07-30 09:28:32 +0700

    commit

---
+++ src/main/java/com/Controller.java
@@ -0,0 +1,43 @@
+package com;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/calculate")
+    public String calculate(@RequestParam(name = "firstOperand", required = false, defaultValue = "0") double firstOperand,
+                            @RequestParam(name = "secondOperand", required = false, defaultValue = "0") double secondOperand,
+                            @RequestParam(name = "operator", required = false, defaultValue = "+") String operator,
+                            ModelMap model) {
+        double result = 0;
+        switch (operator) {
+            case "+": {
+                result = firstOperand + secondOperand;
+                operator = "Addition";
+                break;
+            }
+            case "-": {
+                result = firstOperand - secondOperand;
+                operator = "Subtraction";
+                break;
+            }
+            case "*": {
+                result = firstOperand * secondOperand;
+                operator = "Multiplication";
+                break;
+            }
+            case "/": {
+                result = firstOperand / secondOperand;
+                operator = "Division";
+                break;
+            }
+        }
+        model.addAttribute("result", result);
+        model.addAttribute("operator", operator);
+        model.addAttribute("firstOperand", firstOperand);
+        model.addAttribute("secondOperand", secondOperand);
+        return "index";
+    }
+}
